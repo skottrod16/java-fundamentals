8f0d41a03183466c755af189a95a0bb6d8ce68e4
Kenneth Rothstein <skottrod@me.com>
2023-02-05 15:09:52 -0800

    Challenge 1-Initials

---
+++ YourInitialsChallenge1.java
@@ -0,0 +1,12 @@
+public class YourInitialsChallenge1 {
+    
+    public static void main(String[] args) {
+        System.out.println("K   K   SSS   RRRR   ");
+        System.out.println("K  K   S   S  R   R  ");
+        System.out.println("K K    S      R   R  ");
+        System.out.println("KK      SSS   RRRR   ");
+        System.out.println("K K        S  R R    "); 
+        System.out.println("K  K   S   S  R  R   ");
+        System.out.println("K   k   SSS   R   R  ");
+    }
+}
