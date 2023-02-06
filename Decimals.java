6dffc058af248f7d24edab1bcfd9143b25088a6a
Kenneth Rothstein <skottrod@me.com>
2023-02-06 09:51:19 -0800

    Using double for simple math

---
+++ Decimals.java
@@ -0,0 +1,12 @@
+public class Decimals {
+
+    public static void main(String[] args) {
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up.");
+
+        double people = 20; //always use double for math calculations. This will be stored as a double 20.0000
+        int wallet = 3;
+        System.out.println(people / wallet);
+    }
+    
+}
