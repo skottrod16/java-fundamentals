29b2cf82e495ec75309278ad3806ef896b5aceee
Kenneth Rothstein <skottrod@me.com>
2023-02-05 17:18:27 -0800

    Using char variable

---
+++ Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+
+    public static void main(String[] args) {
+        char potions = 'a';
+        char arithmancy = 'b';
+        char charms = 'c';
+        System.out.println("Potions " + potions);
+        System.out.println("Arithmancy " + arithmancy);
+        System.out.println("Charms " + charms);
+    }
+}
