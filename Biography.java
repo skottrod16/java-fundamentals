00567de7aeec1876913ff66cc1cc6f99c086c1b6
Kenneth Rothstein <skottrod@me.com>
2023-02-05 17:50:22 -0800

    Use different variable types to create a mini biography by joining them

---
+++ Biography.java
@@ -0,0 +1,20 @@
+public class Biography {
+    
+    public static void main(String[] args) {
+        String name = "Kenneth.";
+        int age = 30;
+        String country = "United States of America.";
+        String sport = "Weight Training.";
+        double hours = 1.5;
+        String game = "Grand Theft Auto V.";
+        String subject = "AFJROTC.";
+        char grade = 'A';
+
+        System.out.println("My name is " + name + " I am " + age + " years old, and I am from the " + country);
+        System.out.println("My favorite physical activity is " + sport + " I train for " + hours + " hours a day.");
+        System.out.println("When I am tired, I like to play " + game);
+        System.out.println("In high school, my favorite subject was " + subject + " My grade in the couse was an " + grade + ".");
+
+
+    }
+}
