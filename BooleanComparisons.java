c4a7ee6444e4ca649718b5374e78da61f0ec00cc
Kenneth Rothstein <skottrod@me.com>
2023-02-21 16:34:56 -0800

    Greater than, less than comparisons

---
+++ BooleanComparisons.java
@@ -0,0 +1,16 @@
+public class BooleanComparisons {
+    public static void main(String args[]) {
+        //Greater than & less than comparisons; switch the signs to see them output. 
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+
+        System.out.println(biologyGrade < chemistryGrade);
+
+        double sales = 37.55;
+        double costs = 5.55;
+
+        System.out.println(sales < costs);
+
+    }
+    
+}
