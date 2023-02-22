004999dbb261aac79f3b86e788898b9e3a72d0c3
Kenneth Rothstein <skottrod@me.com>
2023-02-22 07:33:25 -0800

    If Else statements intro

---
+++ IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 65;
+
+        if (grade > 50) {
+            System.out.println("Great job! You passed!");
+        }
+    }
+    
+}
