29fc1f04569657935688ef6bdf0711f64b029382
Kenneth Rothstein <skottrod@me.com>
2023-02-23 12:36:03 -0800

    added else if statements

--- IfElse.java
+++ IfElse.java
@@ -1,12 +1,17 @@
 public class IfElse {
     public static void main(String[] args) {
-        int grade = 50;
+        int grade = 65;
 
-        if (grade >= 50) {
-            System.out.println("Great job! You passed!");
-        }
-        else {
-            System.out.println("Study hard next time!");
+        if (grade >= 80) {
+            System.out.println("You got an A.");
+        } else if (grade >= 70) {
+            System.out.println("You got a B.");
+        } else if (grade >= 60) {
+            System.out.println("You got a C.");
+        } else if (grade >= 50) {
+            System.out.println("You got a D.");
+        } else {
+            System.out.println("You got an F. Study hard next time!");
         }
     }
     
