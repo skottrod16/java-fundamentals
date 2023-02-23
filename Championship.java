62ee70761e93bf43e27f5be1993c13e654fa2ec8
Kenneth Rothstein <skottrod@me.com>
2023-02-23 13:05:58 -0800

    added else if statements

--- Championship.java
+++ Championship.java
@@ -3,17 +3,17 @@ public class Championship {
 
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        int gryffindor = 400;    //gryffindor points
-        int ravenclaw = 200;    //ravenclaw points
-        int margin = (gryffindor - ravenclaw);
+        int gryffindor = 100;    //gryffindor points
+        int ravenclaw = 500;    //ravenclaw points
+        int margin = gryffindor - ravenclaw;
     
         // int margin = amount of points by which gryffindor scored over ravenclaw;
         
-        if (gryffindor >= 300) {
+        if (margin >= 300) {
             System.out.println("Gryffindor takes the house cup!");
-        } else if (gryffindor >= 200) {
+        } else if (margin >= 0) {
             System.out.println("In second place, Gryffindor!");
-        } else if (gryffindor <= 100) { 
+        } else if (margin >= -100) { 
             System.out.println("In thrid place, Gryffindor!");
         } else {
             System.out.println("In fourth place, Gryffindor!");
