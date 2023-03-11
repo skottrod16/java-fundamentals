f825300ee7296c04fd12e6e3161f54a4e8cc8623
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-11 08:11:43 -0800

    Added if else statements

--- WeatherNetwork.java
+++ WeatherNetwork.java
@@ -4,11 +4,16 @@ public class WeatherNetwork {
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
         int temp = 25;  
-        String forecast = "";
 
+        String forecast;
         //IF - ELSE IF - ELSE STATEMENTS HERE!
-        
-        
+        if(temp <= -1){
+            forecast = "It is FREEZING! Stay home!";
+        } else if (temp <= 10) {
+            forecast = "The forecast is chilly. Wear a coat!";
+        } else {
+            forecast = "It is warm outside. Enjoy the beautiful day!";
+        }
         
         System.out.println(forecast);
     }
