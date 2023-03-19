6b249ae1f6ca7a46b5629dca2b46849cc5c8b9a1
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-19 11:58:08 -0700

    Added if else statement with variables

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,5 +1,12 @@
 public class LogicalOperators {
     public static void main(String[] args) {
-        
+        int chemistryGrade = 78;
+        int englishGrade = 65;
+
+        if (chemistryGrade > 75 || englishGrade > 75) {
+            System.out.println("Congratulations, you got the scholarship!");
+        } else {
+            System.out.println("We're sorry, you did not get the scholarship.");
+        }
     }
 }
