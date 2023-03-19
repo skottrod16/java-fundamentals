379be8946d5cff120b9bcb7ea5205b47d2de01ad
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-19 12:04:59 -0700

    Added new variable and comparisson.

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -2,8 +2,9 @@ public class LogicalOperators {
     public static void main(String[] args) {
         int chemistryGrade = 67;
         int englishGrade = 65;
+        String language = "Java";
 
-        if (chemistryGrade > 75 || englishGrade > 75) {
+        if (chemistryGrade > 75 || englishGrade > 75 || language.equals("Java")) {
             System.out.println("Congratulations, you got the scholarship!");
         } else {
             System.out.println("We're sorry, you did not get the scholarship.");
