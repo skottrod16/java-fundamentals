d8f2d5424d745ef44e51c1232464517d66d4c7f1
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-19 12:22:02 -0700

    Added new if else statement, variables and comparisons.

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -9,8 +9,21 @@ public class LogicalOperators {
         } else {
             System.out.println("We're sorry, you did not get the scholarship.");
         }
+
+        int credits = 56;
+        double GPA = 1.4;
+    
+        if (credits >= 40 && GPA >= 2.0) {
+            System.out.println("You earned your diploma.");
+        } else {
+            System.out.println("Sorry, you need at least 40 credits and a minimum GPA of 2.0.");
+        }
     }
+
 }
 
 /* As long as one comparison is True, the ENTIRE condition is True and the first line of code is executed. Otherwise, if NO comparisions are True, then the ENTIRE condition is FALSE and the else statement is executed. 
 */
+
+// OR Operator || (double pipe); returns TRUE if EITHER comparison is true.
+// AND Operator && (double ampersand); returns TRUE if EVERY comparision is true.
