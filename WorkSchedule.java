be78c71b5c1b355bf0113a18e7b2188e97c108eb
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-20 16:31:54 -0700

    added 2 variables and comments.

--- WorkSchedule.java
+++ WorkSchedule.java
@@ -1,5 +1,8 @@
 public class WorkSchedule {
     public static void main(String[] args) {
-        
+        //Assume that day can be any value between 1 (Monday) and 7 (Sunday). 
+        //There's also a boolean variable: holiday.
+        int day = 3;
+        boolean holiday = true;
     }
 }
