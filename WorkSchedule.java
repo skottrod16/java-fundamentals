10168133140a618f5eac16185079d8e7e47742fc
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-20 16:59:36 -0700

    Added if else if statements

--- WorkSchedule.java
+++ WorkSchedule.java
@@ -4,5 +4,13 @@ public class WorkSchedule {
         //There's also a boolean variable: holiday.
         int day = 3;
         boolean holiday = true;
+
+        if (holiday) {
+            System.out.println("Whoo hoo! No work!");
+        } else if (day == 6 || day == 7) {
+            System.out.println("It is the weekend, no work!");
+        } else {
+            System.out.println("Wake up at 7AM! :( )");
+        }
     }
 }
