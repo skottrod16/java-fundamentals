8457feddffa9b49dd906a0b692b670ddb382f768
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-03-20 17:05:34 -0700

    fixed println statement

--- WorkSchedule.java
+++ WorkSchedule.java
@@ -2,15 +2,15 @@ public class WorkSchedule {
     public static void main(String[] args) {
         //Assume that day can be any value between 1 (Monday) and 7 (Sunday). 
         //There's also a boolean variable: holiday.
-        int day = 3;
-        boolean holiday = true;
+        int day = 2;
+        boolean holiday = false;
 
         if (holiday) {
             System.out.println("Whoo hoo! No work!");
         } else if (day == 6 || day == 7) {
             System.out.println("It is the weekend, no work!");
         } else {
-            System.out.println("Wake up at 7AM! :( )");
+            System.out.println("Wake up at 7AM! :(");
         }
     }
 }
