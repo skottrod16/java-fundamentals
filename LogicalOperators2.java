53cac72d9b57e06bca9e252129be668b0e11182b
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-09 17:47:35 -0700

    added more if else statements

--- LogicalOperators2.java
+++ LogicalOperators2.java
@@ -16,25 +16,43 @@ public class LogicalOperators2 {
         // otherwise: System.out.println("You cannot legally drive.");
 
         int age = 25;
-        double income = 45000;
+        double income = 40000;
+
+        if(age >= 21 && income >= 40000) {
+            System.out.println("You are eligible for a loan.");
+        } else {
+            System.out.println("You are not eligible for a loan.");
+        }
 
         // is at least 21 and has an income of at least 40000 : System.out.println("You are eligible for a loan.");
         // otherwise: System.out.println("You are not eligible for a loan.");
 
 
         String inputUsername = "JohnDoe";
-        String inputPassword = "password123";
+        String inputPassword = "Password123";
 
         String correctUsername = "JohnDoe";
         String correctPassword = "password123";
+
+        if(inputUsername.equals(correctUsername) && inputPassword.equals(correctPassword)) {
+            System.out.println("Access granted!");
+        } else {
+            System.out.println("Invalid username or password.");
+        }
 
         // the inputted username and password are correct: System.out.println("Access granted!");
         // otherwise: System.out.println("Invalid username or password");
 
         /**********************OR OPERATOR**********************/
 
-        boolean hasGoodPerformance = true;
+        boolean hasGoodPerformance = false;
         boolean isLongTermEmployee = false;
+
+        if(hasGoodPerformance || isLongTermEmployee) {
+            System.out.println("The user is eligible for a promotion.");
+        } else {
+            System.out.println("The user is not eligible for a promotion.");
+        }
 
         // good performance or is a long term employee: System.out.println("The user is eligible for a promotion.");
         // otherwise: System.out.println("The user is not eligible for a promotion.");
