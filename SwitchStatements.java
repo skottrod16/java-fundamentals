c41e673528c35f7434231ced0889665c61c2369d
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-11 03:00:59 -0700

    Added if else if statments to Section 3

--- SwitchStatements.java
+++ SwitchStatements.java
@@ -2,24 +2,50 @@ public class SwitchStatements {
     public static void main(String[] args) {
 
         // Section 1: Weather conditions
-        String weather = "sunny";
+        String weather = "rainy";
+        switch (weather) {
+            case "sunny": System.out.println("You can wear a shirt!"); break;
+            case "cloudy": System.out.println("You ushould wear a sweater!"); break;
+            case "rainy": System.out.println("You should wear a raincoat!"); break;
+            case "snowy":System.out.println("You should wear a jacket!"); break;
+            default: System.out.println("You can wear whatever you want!");
+        }
         // Determine what to wear based on the weather (e.g., sunny, cloudy, rainy, snowy)
         // Decide whether to use if-else or switch
 
         // Section 2: User role
         int role = 2;
+        switch (role) {
+            case 1: System.out.println("You are an admin"); break;
+            case 2: System.out.println("You are an editor"); break;
+            case 3: System.out.println("You are a user"); break;
+            default: System.out.println("Please contact OPs Tech IT");
+        }
         // Determine user access level based on the role (e.g., 1: admin, 2: editor, 3: user)
         // Decide whether to use if-else or switch
 
         // Section 3: Temperature and humidity
         int temperature = 75;
         int humidity = 65;
+
+        if(temperature >= 80 && humidity >= 60) {
+            System.out.println("It is too hot and humid\n");
+        } else if (temperature >= 80) {
+            System.out.println("It's too hot\n");
+        } else if (temperature <= 60 && humidity >= 60) {
+            System.out.println("It is too hot and humid.");
+        } else if (temperature <= 60) {
+            System.out.println("It is too cold");
+        } else {
+            System.out.println("It is comfortable");
+        }
         // Determine the comfort level based on both temperature and humidity (e.g., too hot, too cold, comfortable)
         // Decide whether to use if-else or switch
 
         // Section 5: Age and income
         int age = 25;
         int income = 50000;
+        
         // Determine eligibility for a specific credit card based on age and income
         // Decide whether to use if-else or switch
 
