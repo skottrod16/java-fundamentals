ef7fd779e8854a612765c037c78c00878c89b473
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-15 09:23:07 -0700

    removed options a and b comments & code that were not needed.

--- Dealership.java
+++ Dealership.java
@@ -34,39 +34,21 @@ public class Dealership {
                 }
 
                 break;
-                
-            case "b": System.out.println("You chose option " + option); break;
+
+            case "b": System.out.println("You chose option " + option); 
+            System.out.println("What is your car valued at?");
+            int value = scan.nextInt();
+            System.out.println("What is your selling price?");
+            int price = scan.nextInt();
+            
+            if (value > price && price < 30000) {
+                System.out.println("We will buy your car. Pleasure doing business with you!");
+            } else {
+                System.out.println("Sorry, we're not interested.");
+            }
+            break;
             default: System.out.println("Invalid option " + option);             
         }
-
-        /******************Case A******************/
-
-        System.out.println("What is your budget?");
-
-        // budget greater than or equal to 10000
-            System.out.println("Great! A Nissan Altima is available");
-            System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
-            String insurance = scan.nextLine();
-            System.out.println("\nDo you have a license? Write 'yes' or 'no'");
-            String license = scan.nextLine();
-            System.out.println("What is your credit score?");
-            int creditScore = scan.nextInt();
-            // if they have insurace, a license, and credit score greater than 660
-                System.out.println("Sold! Pleasure doing business with you");
-
-        // they don't meet budget requirements
-                System.out.println("We're sorry. You are not eligible");
-
-        /******************Case B******************/
-        System.out.println("What is your car valued at?");
-        int value = scan.nextInt();
-        System.out.println("What is your selling price?");
-        int price = scan.nextInt();
-
-        // value is greater than price and price is less than 30000
-            System.out.println("We will buy your car. Pleasure doing business with you!");
-        //otherwise
-            System.out.println("Sorry, we're not interested.");
 
         scan.close();
         }
