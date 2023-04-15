25473041a64846448c41e4b94da1fcd3a69b653b
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-15 09:18:03 -0700

    Case a coded with if else statements for licencse, credit score etc

--- Dealership.java
+++ Dealership.java
@@ -9,7 +9,32 @@ public class Dealership {
         String option = scan.nextLine();
 
         switch (option) {
-            case "a": System.out.println("You chose option " + option); break;
+            case "a": System.out.println("You chose option " + option); 
+                System.out.println("What is your budget?");
+                double budget = scan.nextDouble();
+                if (budget >= 10000) {
+                    System.out.println("Great! A Nissan Altima is available");
+                    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a license? Write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    System.out.println("What is your credit score?");
+                    int creditScore = scan.nextInt();
+
+                    if (insurance.equals("yes")&& license.equals("yes") && creditScore > 660) {
+                        System.out.println("Sold! Pleasure doing business with you");
+                    } else {
+                        System.out.println("Sorry, we cannot sell you a car.");
+                    }
+
+
+                } else {
+                    System.out.println("We're sorry. You are not eligible");
+                }
+
+                break;
+                
             case "b": System.out.println("You chose option " + option); break;
             default: System.out.println("Invalid option " + option);             
         }
