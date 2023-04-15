dd0273ae234543f9ca1b0e1b13327c32896ff1f6
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-15 12:03:21 -0700

    Removed unnecessary comments

--- Bank.java
+++ Bank.java
@@ -4,14 +4,10 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         
-        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
-
         Scanner scan = new Scanner(System.in);
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
-        //Task 1 - Pick up the user's decision.
         String decision = scan.nextLine();
-        //Task 2 - Print this if the decision is "yes"
         if(decision.equals("yes")) {
             System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
@@ -33,7 +29,7 @@ public class Bank {
         } else {
             System.out.println("\nOK, have a glorious day!");
         }
-        
+
         scan.close();
     }
 }
