363f79585ad685a3f6a2c3bfc7d3427fa6b7c139
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-15 11:55:19 -0700

    Completed mini program by adding scanner inputs, variables & if else statements

--- Bank.java
+++ Bank.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Bank {
     public static void main(String[] args) {
         
@@ -10,27 +11,29 @@ public class Bank {
         System.out.println("Are you here to get a mortgage? (yes or no)");
         //Task 1 - Pick up the user's decision.
         String decision = scan.nextLine();
-
         //Task 2 - Print this if the decision is "yes"
-        System.out.println("\nGreat! In one line" +
+        if(decision.equals("yes")) {
+            System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
-            System.out.println("\nHow many years have you worked for?");
-               // Task 4 - Pick up number of years
-
+            double savings = scan.nextDouble();
+            double creditCardDebt = scan.nextDouble();
+            System.out.println("\nHow many years have you worked?");
+            int yearsWorked = scan.nextInt();
+            scan.nextLine();
             System.out.println("What is your name?");
-               // Task 5 - Pick up the user's name
-                
-               //Task 6 - Approve the mortgage if they meet the requirements (see article)
-               //       - Otherwise, don't give them a mortgage.          
-               System.out.println("Congratulations <name> You have been approved!");
-               System.out.println("Sorry, you are not eligible for a mortgage.");
+            String name = scan.nextLine();
 
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
+            if (savings >= 10000 && creditCardDebt <= 5000 && yearsWorked > 2) {
+                System.out.println("Congratulations " + name + "!" + "You have been approved!");
+            } else {
+                System.out.println("Sorry, you are not eligible for a morgage with ROYAL BANK OF JAVA.");
+            }
 
-
+        } else {
+            System.out.println("\nOK, have a glorious day!");
+        }
+        
         scan.close();
     }
 }
