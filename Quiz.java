7dbe581acc25cb224298dbb0df440ba4d2f7e34b
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-16 12:27:13 -0700

    added scanner input for answers 1-4

--- Quiz.java
+++ Quiz.java
@@ -11,18 +11,22 @@ public class Quiz {
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
         //store answer 1
+        String answer1 = scan.next();
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
         //store answer 2
+        String answer2 = scan.next();
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
         //store answer 3
+        String answer3 = scan.next();
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
         //store answer 4
+        String answer4 = scan.next();
 
         int score = 0;
 
