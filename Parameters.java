9edef8fd8bb65cc72d9372db33d1a78da24d4915
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-18 16:58:05 -0700

    Created function calculateArea & called function by passing matching values.

--- Parameters.java
+++ Parameters.java
@@ -1,5 +1,9 @@
 public class Parameters {
     public static void main(String[] args) {
-        
+        calculateArea(2.3, 3.6);
+    }
+
+    public static void calculateArea(double length, double width) {
+
     }
 }
