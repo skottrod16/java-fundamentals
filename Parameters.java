1fc488a511da60d3c7eba9549765a25b795f145c
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-18 17:07:37 -0700

    Added 2 more arguments

--- Parameters.java
+++ Parameters.java
@@ -1,6 +1,8 @@
 public class Parameters {
     public static void main(String[] args) {
-        calculateArea(2.3, 3.6); //2.3 and 3.6 are arguments while:
+        calculateArea(2.3, 3.6);
+        calculateArea(1.6, 2.4);
+        calculateArea(2.6, 4.2); //2.3 and 3.6 are arguments while:
     }
 
     public static void calculateArea(double length, double width) { //double length and double width are parameters.
@@ -8,3 +10,8 @@ public class Parameters {
         System.out.println("Area: " + area);
     }
 }
+
+/*
+ -A parameter stores a value that your function expects to receive.
+ -
+ */
