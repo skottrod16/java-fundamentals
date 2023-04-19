94ae42c6113002858692bf2ff52bd22bb41d357d
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-18 17:01:03 -0700

    Added single line comments and calling the function to perform a task.

--- Parameters.java
+++ Parameters.java
@@ -1,9 +1,10 @@
 public class Parameters {
     public static void main(String[] args) {
-        calculateArea(2.3, 3.6);
+        calculateArea(2.3, 3.6); //2.3 and 3.6 are arguments while:
     }
 
-    public static void calculateArea(double length, double width) {
-
+    public static void calculateArea(double length, double width) { //double length and double width are parameters.
+        double area = length * width ;
+        System.out.println("Area: " + area);
     }
 }
