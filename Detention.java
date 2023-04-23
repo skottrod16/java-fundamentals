efb8c2842ab84d818d2f85724aa1f5ba9680cd62
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-23 14:46:55 -0700

    final code

--- Detention.java
+++ Detention.java
@@ -10,7 +10,7 @@ public class Detention {
         
 
    }
-   public static void printLines() {
+  public static void printLines() {
     System.out.println("I will not copy and paste code.");
     System.out.println("I will use Camel Case when writing class names.");
     System.out.println("I will use lower Camel Case when writing function names.");
