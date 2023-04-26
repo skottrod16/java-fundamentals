cca7c0f0ab6c3b353e27bb754016b8c8ebf26e95
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-26 15:20:07 -0700

    Created a switch case'

--- ReturnValues.java
+++ ReturnValues.java
@@ -12,6 +12,12 @@ public class ReturnValues {
 
     public static String explainArea(String language) {
         
+        switch (language) {
+            case "English": return "Area equals length * width";
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "area es igual a largo * ancho";
+            default: return "Language not available";
+        }
     }
     
 }
