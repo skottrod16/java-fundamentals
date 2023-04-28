b3f534b3982be0cf7e23bd8f5d8e0239b28003f4
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-28 15:40:52 -0700

    Added doc comment for greet method

--- DocComments.java
+++ DocComments.java
@@ -4,7 +4,12 @@ public class DocComments {
     }
 
 
-
+    /** 
+     * Function name: greet
+     * 
+     * Inside the function:
+     *  1. prints: 'Hi'
+    */
 
     public static void greet() {
         System.out.println("Hi");
