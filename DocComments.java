4a1908caf06a2e33dda7979385b8d42fc7e58bdb
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-04-28 15:44:09 -0700

    Added doc comment for printText method

--- DocComments.java
+++ DocComments.java
@@ -14,4 +14,18 @@ public class DocComments {
     public static void greet() {
         System.out.println("Hi");
     }
+
+    /**
+     * Function name: printText
+     * @param name (String)
+     * @param age (String)
+     * 
+     * 
+     * Inside the function:
+     *  1. prints the name and age as part of a text
+     * 
+     */
+    public static void printText(String name, String age) {
+        System.out.println("Hi, I'm " + name + " and I'm " + age + " years old.");
+    }
 }
