002d236d281f070d13dcc7c3f25bf1d30df6dfd7
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-06-09 13:32:28 -0700

    Created rollDice method and call to method

--- DiceJack.java
+++ DiceJack.java
@@ -1,5 +1,17 @@
 public class DiceJack {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int roll1 = rollDice();
+        int roll2 = rollDice();
+        int roll3 = rollDice();
 
+        System.out.println(roll1);
+        System.out.println(roll2);
+        System.out.println(roll3);
+        
+    }
+    public static int rollDice() {
+        double randomNumber = Math.random() * 6;
+        randomNumber += 1;
+        return (int)randomNumber; // 1-6
     }
 }
