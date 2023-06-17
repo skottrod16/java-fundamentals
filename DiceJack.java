abf87b75c21a2f800c34e2068baab38efa1a47ea
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-06-17 07:03:07 -0700

    Added scanner function

--- DiceJack.java
+++ DiceJack.java
@@ -1,8 +1,13 @@
 public class DiceJack {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int roll1 = rollDice();
         int roll2 = rollDice();
         int roll3 = rollDice();
+
+        System.out.print("Enter three numbers between 1 and 6: ");
+        in num1 = scanner.nextInt();
+
 
         System.out.println(roll1);
         System.out.println(roll2);
