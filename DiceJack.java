49c842fb0df8462fbe63db28f4f821ba15274565
Kenneth Rothstein <kenneth.rothstein@icloud.com>
2023-06-17 07:07:18 -0700

    Created areLessThan1 method

--- DiceJack.java
+++ DiceJack.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DiceJack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -6,13 +8,20 @@ public class DiceJack {
         int roll3 = rollDice();
 
         System.out.print("Enter three numbers between 1 and 6: ");
-        in num1 = scanner.nextInt();
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
 
 
         System.out.println(roll1);
         System.out.println(roll2);
         System.out.println(roll3);
         
+    }
+
+    public static boolean areLessThan1(int num1, int num2, int num3) {
+        return (num1 < 1 || num2 < 1 || num3 < 1);
     }
     public static int rollDice() {
         double randomNumber = Math.random() * 6;
